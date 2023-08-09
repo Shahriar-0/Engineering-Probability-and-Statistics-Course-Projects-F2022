6744be0d9cb0453980cbee77a9c19647f1229d84
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-08-09 22:48:33 +0330

    hypergeometric  vs binomial distribution for fun

---
+++ 1/hypergeometric&binomial.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy.special import comb
+import matplotlib.pyplot as plt
+
+N = 100
+n = 50
+M = 20
+k = np.arange(0, M, 1)
+
+fig, ax = plt.subplots(1, 2)
+
+y = comb(M, k) * comb(N - M, n - k) / comb(N, n)
+ax[0].plot(k, y)
+
+y2 = comb(n, k) * ((M / N) ** k) * (((N - M) / N) ** (n - k))
+ax[0].plot(k, y2)
+
+n = 10
+y = comb(M, k) * comb(N - M, n - k) / comb(N, n)
+ax[1].plot(k, y)
+
+y2 = comb(n, k) * ((M / N) ** k) * (((N - M) / N) ** (n - k))
+ax[1].plot(k, y2)
+
+plt.show()
+hypergeometric 

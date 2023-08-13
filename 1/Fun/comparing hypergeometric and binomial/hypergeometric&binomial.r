97053d43969504ce65dfb5c5fa48ef72b8737ea7
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-08-14 01:13:06 +0330

    hw1 fix

--- 1/Fun/comparing hypergeometric and binomial/hypergeometric&binomial.r
+++ 1/Fun/comparing hypergeometric and binomial/hypergeometric&binomial.r
@@ -1,25 +1,26 @@
+library(ggplot2)
 N <- 100
 n <- 50
 M <- 20
 k <- seq(0, M - 1, 1)
 
 y <- choose(M, k) * choose(N - M, n - k) / choose(N, n)
-y2 <- choose(n, k) * ((M / N) ^ k) * (((N - M) / N) ^ (n - k))
+y2 <- choose(n, k) * ((M / N)^k) * (((N - M) / N)^(n - k))
 
 df1 <- data.frame(k = k, y = y, y2 = y2)
 
 n <- 10
 y <- choose(M, k) * choose(N - M, n - k) / choose(N, n)
-y2 <- choose(n, k) * ((M / N) ^ k) * (((N - M) / N) ^ (n - k))
+y2 <- choose(n, k) * ((M / N)^k) * (((N - M) / N)^(n - k))
 
 df2 <- data.frame(k = k, y = y, y2 = y2)
 
-p1 <- ggplot2::ggplot(df1, aes(x = k)) +
-    geom_line(aes(y = y, color = "#ff8800")) +
-    geom_line(aes(y = y2, color = "blue"))
+p1 <- ggplot(df1, aes(x = k)) +
+  geom_line(aes(y = y, color = "#ff8800")) +
+  geom_line(aes(y = y2, color = "blue"))
 
-p2 <- ggplot2::ggplot(df2, aes(x = k)) +
-    geom_line(aes(y = y, color = "#ff8800")) +
-    geom_line(aes(y = y2, color = "blue"))
+p2 <- ggplot(df2, aes(x = k)) +
+  geom_line(aes(y = y, color = "#ff8800")) +
+  geom_line(aes(y = y2, color = "blue"))
 
 gridExtra::grid.arrange(p1, p2)
